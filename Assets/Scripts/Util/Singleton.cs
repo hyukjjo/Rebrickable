6f144de45b2142c691e6d6cc7eb1919d00bf872c
hyukjjo <hyukjjo@gmail.com>
2022-09-21 16:32:53 +0900

    add: Singleton.cs
    범용적으로 사용 가능한 싱글톤 스크립트 추가

---
+++ Assets/Scripts/Util/Singleton.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance 
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = (T)FindObjectOfType(typeof(T));
+                
+                if(_instance == null)
+                {
+                    var singletonObject = new GameObject();
+                    _instance = singletonObject.AddComponent<T>();
+                    singletonObject.name = $"{typeof(T).ToString()} (Singleton)";
+                    DontDestroyOnLoad(singletonObject);
+                }
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = (T)FindObjectOfType(typeof(T));
+
+            if (_instance == null)
+            {
+                var singletonObject = new GameObject();
+                _instance = singletonObject.AddComponent<T>();
+                singletonObject.name = $"{typeof(T).ToString()} (Singleton)";
+                DontDestroyOnLoad(singletonObject);
+            }
+            else
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+    }
+}
