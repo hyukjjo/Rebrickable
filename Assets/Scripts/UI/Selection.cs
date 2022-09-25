4cafd553ffb755302be29d1d73af004e8c45eac3
hyukjjo <48501630+hyukjjo@users.noreply.github.com>
2022-09-25 21:35:37 +0900

    Update Selection.cs

--- Assets/Scripts/UI/Selection.cs
+++ Assets/Scripts/UI/Selection.cs
@@ -1,9 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct sSelectionModel
+{
+    public string modelName;
+    public Action OnSelect;
+};
+
 public class Selection : MonoBehaviour
 {
     [Header("Model")]
-    [SerializeField] private GameObject _temp; 
+    [SerializeField] private List<GameObject> _selectionModels = new List<GameObject>();
+
+    [Header("View")]
+    [SerializeField] private GameObject _characterView;
 }
