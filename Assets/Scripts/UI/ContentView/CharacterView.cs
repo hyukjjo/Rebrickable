f9c5b910764b8357d180b0933b96826181f866e0
hyukjjo <hyukjjo@gmail.com>
2022-09-28 17:09:37 +0900

    refac: UI View 구조 변경
    CharacterView의 상위 클래스인
    ContentView 생성

--- Assets/Scripts/UI/ContentView/CharacterView.cs
+++ Assets/Scripts/UI/ContentView/CharacterView.cs
@@ -1,28 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
-using UnityEngine.UI;
 
-public class CharacterView : MonoBehaviour //View를 공통적으로 사용하기 위해서는 상위 클래스로 올리고 CharacterView가 상속받아야할 듯. 일단은 테스트 22.09.27
+public class CharacterView : ContentView
 {
-    [Serializable]
-    public class Sprites
+    public override void Init(Sprites sprites = null, Action onClick = null, Action onHovered = null)
     {
-        public Sprite normalSprite;
-        public Sprite hoverSprite;
-    }
-
-    [SerializeField] private Image _image;
-    [SerializeField] private Sprites _sprites;
-    private Action _onClicked;
-    private Action _onHovered;
-
-    public void Init(Sprites sprites = null, Action onClick = null, Action onHovered = null)
-    {
-        _onClicked = onClick;
-        _onHovered = onHovered;
-        _sprites = sprites ?? _sprites;
-        _image.sprite = _sprites.normalSprite;
+        base.Init(sprites, onClick, onHovered);
     }
 }
