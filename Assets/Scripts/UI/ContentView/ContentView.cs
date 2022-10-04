e51505fffca9eb0aa174dddb662caebf7df74fdf
hyukjjo <48501630+hyukjjo@users.noreply.github.com>
2022-10-04 23:18:47 +0900

    fix: 구조 변경에 따른 씬, 스크립트 수정
    ContentView.cs
    CharacterView.cs
    Starter씬

--- Assets/Scripts/UI/ContentView/ContentView.cs
+++ Assets/Scripts/UI/ContentView/ContentView.cs
@@ -1,48 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class ContentView : Selectable //View�� ���������� ����ϱ� ���ؼ��� ���� Ŭ������ �ø��� CharacterView�� ��ӹ޾ƾ��� ��. �ϴ��� �׽�Ʈ 22.09.27
+namespace Starter.ContentView
 {
-    [Serializable]
-    public class Sprites
+    public class ContentView : Selectable //View�� ���������� ����ϱ� ���ؼ��� ���� Ŭ������ �ø��� CharacterView�� ��ӹ޾ƾ��� ��. �ϴ��� �׽�Ʈ 22.09.27
     {
-        public Sprite normalSprite;
-        public Sprite hoverSprite;
-    }
+        [Serializable]
+        public class Sprites
+        {
+            public Sprite normalSprite;
+            public Sprite hoverSprite;
+        }
 
-    [SerializeField] private Image _image;
-    [SerializeField] private Sprites _sprites;
-    [SerializeField] private Button button;
-    private Action _onClicked;
-    private Action _onHovered;
+        [SerializeField] private Image _image;
+        [SerializeField] private Sprites _sprites;
+        [SerializeField] private Button button;
+        private Action _onClicked;
+        private Action _onHovered;
 
-    public virtual void Init(Sprites sprites = null, Action onClick = null, Action onHovered = null)
-    {
-        _onClicked = onClick;
-        _onHovered = onHovered;
-        _sprites = sprites ?? _sprites;
-        _image.sprite = _sprites.normalSprite;
-        button.onClick.AddListener(OnClicked);
-    }
+        public virtual void Init(Sprites sprites = null, Action onClick = null, Action onHovered = null)
+        {
+            _onClicked = onClick;
+            _onHovered = onHovered;
+            _sprites = sprites ?? _sprites;
+            _image.sprite = _sprites.normalSprite;
+            button.onClick.AddListener(OnClicked);
+        }
 
-    public virtual void OnClicked()
-    {
-        _onClicked?.Invoke();
-    }
+        public virtual void OnClicked()
+        {
+            _onClicked?.Invoke();
+        }
 
-    public override void OnPointerEnter(PointerEventData eventData)
-    {
-        base.OnPointerEnter(eventData);
-        Debug.Log("Pointer Enter");
-    }
+        public override void OnPointerEnter(PointerEventData eventData)
+        {
+            base.OnPointerEnter(eventData);
+            Debug.Log("Pointer Enter");
+        }
 
-    public override void OnPointerExit(PointerEventData eventData)
-    {
-        base.OnPointerExit(eventData);
-        Debug.Log("Pointer Enter");
+        public override void OnPointerExit(PointerEventData eventData)
+        {
+            base.OnPointerExit(eventData);
+            Debug.Log("Pointer Enter");
+        }
     }
 }
