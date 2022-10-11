5760d775ca25bd1cf8d58c757e45c330e2e46b42
hyukjjo <48501630+hyukjjo@users.noreply.github.com>
2022-10-11 23:49:06 +0900

    Update GameManager.cs
    씬 변수 추가

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utility;
 
 public class GameManager : Singleton<GameManager>
 {
+    public ScenePath _sceneToLoad;
+
     // Start is called before the first frame update
     void Start()
     {
