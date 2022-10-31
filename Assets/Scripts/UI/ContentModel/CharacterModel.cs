afbc459d34abd7f823293fc4d71040cb22ef3391
hyukjjo <hyukjjo@gmail.com>
2022-10-31 17:21:00 +0900

    Update CharacterModel.cs

--- Assets/Scripts/UI/ContentModel/CharacterModel.cs
+++ Assets/Scripts/UI/ContentModel/CharacterModel.cs
@@ -7,8 +7,10 @@ using Starter.ContentModel;
 public class CharacterModel : ContentModel
 {
     [SerializeField] private ContentView.Sprites _sprites;
+    [SerializeField] private Player _player;
 
     public ContentView.Sprites Sprites => _sprites;
+    public Player Player => _player;
 
     
 }
