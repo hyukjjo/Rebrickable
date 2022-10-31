5aa0867310a3f29042740525e90b689591d74c04
hyukjjo <hyukjjo@gmail.com>
2022-10-31 17:20:58 +0900

    Update GameManager.cs

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -6,6 +6,7 @@ using Utility;
 public class GameManager : Singleton<GameManager>
 {
     public bool LoadSceneReady = false;
+    public Player Player;
 
     // Start is called before the first frame update
     void Start()
