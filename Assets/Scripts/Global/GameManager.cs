799e75d5240a1830210da9da0b5d1958fb25c578
hyukjjo <hyukjjo@gmail.com>
2022-11-02 18:13:21 +0900

    Update GameManager.cs

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -8,18 +8,6 @@ public class GameManager : Singleton<GameManager>
     public bool LoadSceneReady = false;
     public GameObject PlayerPrefab;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void SaveDataAndExitGame()
     {
         Debug.Log("Save data and exit this game.");
