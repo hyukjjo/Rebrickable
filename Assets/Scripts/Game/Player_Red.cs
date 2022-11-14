1594a697bfe5ec19f516529f0a70006bd404406a
hyukjjo <48501630+hyukjjo@users.noreply.github.com>
2022-11-14 14:21:52 +0900

    Update Player_Red.cs

--- Assets/Scripts/Game/Player_Red.cs
+++ Assets/Scripts/Game/Player_Red.cs
@@ -7,7 +7,7 @@ public class Player_Red : Player
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Player_Red Start");
     }
 
     // Update is called once per frame
