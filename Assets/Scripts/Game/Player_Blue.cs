91741376dedb0c22aefa4bd7613b3f8241003459
hyukjjo <48501630+hyukjjo@users.noreply.github.com>
2022-11-14 14:21:49 +0900

    Update Player_Blue.cs

--- Assets/Scripts/Game/Player_Blue.cs
+++ Assets/Scripts/Game/Player_Blue.cs
@@ -7,7 +7,7 @@ public class Player_Blue : Player
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Player_Blue Start");
     }
 
     // Update is called once per frame
