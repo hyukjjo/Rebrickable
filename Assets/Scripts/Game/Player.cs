28bf9aad4652674e7458d0e56d8e0a2cb057700b
hyukjjo <48501630+hyukjjo@users.noreply.github.com>
2022-11-20 23:15:15 +0900

    Update Player.cs

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -7,10 +7,4 @@ public class Player : MonoBehaviour
 {
     public int Hp;
     public int Speed;
-
-    private void Update()
-    {
-        if (Input.GetKey(KeyCode.UpArrow))
-            transform.Translate(Vector3.up, Space.World);
-    }
 }
