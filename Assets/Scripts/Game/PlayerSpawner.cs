c283a57ce7182f5fdd9036f55f50790363478d17
hyukjjo <hyukjjo@gmail.com>
2022-12-07 18:26:42 +0900

    chore: 임시 코드 추가
    확인 후 지울것

--- Assets/Scripts/Game/PlayerSpawner.cs
+++ Assets/Scripts/Game/PlayerSpawner.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerSpawner : MonoBehaviour
 {
+    PlayerData playerData;
+
     // Start is called before the first frame update
     void Start()
     {
         SpawnPlayer();
+        playerData = new PlayerData();
+        playerData._gold = 0;
+        playerData._level = 0;
+        PlayerData.SetPlayerData(playerData);
     }
 
     // Update is called once per frame
