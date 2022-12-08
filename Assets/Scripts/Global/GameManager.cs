6bbc3eec95923c0db418b087434f312eebb1d2ea
hyukjjo <48501630+hyukjjo@users.noreply.github.com>
2022-12-08 23:47:18 +0900

    Update GameManager.cs
    데이터 저장 함수 생성

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -8,8 +8,23 @@ public class GameManager : Singleton<GameManager>
     public bool LoadSceneReady = false;
     public GameObject PlayerPrefab;
 
+    private int _gold;
+    private int _level;
+
+    public void SaveGold(int gold)
+    {
+        _gold = gold;
+    }
+
+    public void SaveLevel(int level)
+    {
+        _level = level;
+    }
+
     public void SaveDataAndExitGame()
     {
         Debug.Log("Save data and exit this game.");
+
+        PlayerData.SetPlayerData(new PlayerData() {_gold = _gold, _level = _level });
     }
 }
