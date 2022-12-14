d78c3f42583c1adad4d719a918dfa73f553a6369
hyukjjo <hyukjjo@gmail.com>
2022-12-14 18:24:52 +0900

    Update ObjectPool.cs

--- Assets/Scripts/Item/ObjectPool.cs
+++ Assets/Scripts/Item/ObjectPool.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,14 +9,28 @@ public class ObjectPool : Singleton<ObjectPool>
     private GameObject _poolingItemPrefab;
     [SerializeField]
     private GameObject _poolingGoldPrefab;
+    [SerializeField]
+    private GameObject _poolingMonsterPrefab;
     private Queue<Item> _poolingItemQueue = new Queue<Item>();
     private Queue<Gold> _poolingGoldQueue = new Queue<Gold>();
+    private Queue<Monster> _poolingMonsterQueue = new Queue<Monster>();
 
     private void Awake()
     {
         InitItem(10);
         InitGold(10);
+        InitMonster(20);
     }
+
+    private void InitMonster(int initCount)
+    {
+        for (int i = 0; i < initCount; i++)
+        {
+            _poolingMonsterQueue.Enqueue(CreateNewMonster());
+        }
+    }
+
+   
 
     private void InitItem(int initCount)
     {
@@ -59,6 +74,8 @@ public class ObjectPool : Singleton<ObjectPool>
         }
     }
 
+    
+
     public static void ReturnItem(Item obj)
     {
         obj.gameObject.SetActive(false);
@@ -97,5 +114,38 @@ public class ObjectPool : Singleton<ObjectPool>
         obj.gameObject.SetActive(false);
         obj.transform.SetParent(Instance.transform);
         Instance._poolingGoldQueue.Enqueue(obj);
+    }
+
+    private Monster CreateNewMonster()
+    {
+        var newObj = Instantiate(_poolingMonsterPrefab).GetComponent<Monster>();
+        newObj.gameObject.SetActive(false);
+        newObj.transform.SetParent(transform);
+        return newObj;
+    }
+
+    public static Monster TakeMonster()
+    {
+        if (Instance._poolingMonsterQueue.Count > 0)
+        {
+            var obj = Instance._poolingMonsterQueue.Dequeue();
+            obj.transform.SetParent(null);
+            obj.gameObject.SetActive(true);
+            return obj;
+        }
+        else
+        {
+            var newObj = Instance.CreateNewMonster();
+            newObj.gameObject.SetActive(true);
+            newObj.transform.SetParent(null);
+            return newObj;
+        }
+    }
+
+    public static void ReturnMonster(Monster obj)
+    {
+        obj.gameObject.SetActive(false);
+        obj.transform.SetParent(Instance.transform);
+        Instance._poolingMonsterQueue.Enqueue(obj);
     }
 }
