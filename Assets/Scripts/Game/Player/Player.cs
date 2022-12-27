ab5fd7b34de1fe5c8a33a24706cfb8cff66da099
hyukjjo <hyukjjo@gmail.com>
2022-12-27 17:03:25 +0900

    add: IPlayerUniqueSkill
    Player.cs 내부에 플레이어 고유스킬 인터페이스 생성

--- Assets/Scripts/Game/Player/Player.cs
+++ Assets/Scripts/Game/Player/Player.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+public interface IPlayerUniqueSkill
+{
+    public void UsePlayerUniqueSkill();
+}
+
 public class Player : MonoBehaviour
 {
     public int Hp;
