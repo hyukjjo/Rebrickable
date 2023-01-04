722d23f5dd77a72b555632427ad6b954df18e275
hyukjjo <hyukjjo@gmail.com>
2023-01-04 16:48:04 +0900

    update: MonsterSpawner.cs
    오브젝트 풀 변경에 따른 수정사항 반영

--- Assets/Scripts/Game/Monster/MonsterSpawner.cs
+++ Assets/Scripts/Game/Monster/MonsterSpawner.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class MonsterSpawner : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        InitSpawnMonster();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void InitSpawnMonster()
-    {
+    void Start()                                           
+    {                                                      
+        InitSpawnMonster();                                
+    }                                                      
+                                                           
+    // Update is called once per frame                     
+    void Update()                                          
+    {                                                      
+                                                           
+    }                                                      
+                                                           
+    void InitSpawnMonster()        
+    {                              
         for(int i = 0; i < 10; i++)
         {
-            var monster = ObjectPool.TakeMonster();
+            var monster = ObjectPoolManager.Instance.Spawn("Monster_Lv1");
             monster.transform.position = new Vector2(Random.Range(-20, 20), Random.Range(-20, 20));
         }
     }
