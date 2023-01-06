6a49e859334754ba9bfadd188dd9ee121161c87c
hyukjjo <hyukjjo@gmail.com>
2023-01-06 16:56:57 +0900

    Update PlayerSkill_Default_Black.cs

--- Assets/Scripts/Game/Player/Skill/PlayerSkill_Default_Black.cs
+++ Assets/Scripts/Game/Player/Skill/PlayerSkill_Default_Black.cs
@@ -4,37 +4,9 @@ using UnityEngine;
 
 public class PlayerSkill_Default_Black : PlayerSkill
 {
-    private PoolObject skill;
-
     public override void Start()
     {
         base.Start();
-    }
-
-    public override void Update()
-    {
-        base.Update();
-    }
-
-    public override IEnumerator PlayerSkillCoroutine()
-    {
-        float time = 1f;
-        float elapse = 0f;
-        Vector3 targetPosition = _mousePosition - _playerPosition;
-        Vector3 targetDirection = targetPosition / targetPosition.magnitude;
-
-        skill = ObjectPoolManager.Instance.Spawn("PlayerSkill_Default_Black");
-
-        skill.transform.position = _playerPosition;
-
-        while (elapse <= time)
-        {
-            skill.transform.Translate(targetDirection * MoveSpeed, Space.World);
-            elapse += Time.deltaTime;
-            yield return null;
-        }
-
-        ObjectPoolManager.Instance.Despawn(skill);
     }
 
     public override void UseSkill()
@@ -45,6 +17,5 @@ public class PlayerSkill_Default_Black : PlayerSkill
     public override void StopSkill()
     {
         base.StopSkill();
-        //ObjectPoolManager.Instance.Despawn(skill);
     }
 }
