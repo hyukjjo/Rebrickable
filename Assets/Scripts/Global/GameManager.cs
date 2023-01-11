b727103ae6ca8a75bbd904bc69b4470668a45b50
hyukjjo <hyukjjo@gmail.com>
2023-01-11 17:08:38 +0900

    update: GameManager.cs
    게임 종료될때 플레이어 데이터 저장하는 부분 수정

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -9,28 +9,15 @@ public class GameManager : Singleton<GameManager>
     public GameObject PlayerPrefab;
     public GameObject currentPlayer;
 
-    private int _gold;
-    private int _level;
-
     private void Start()
     {
         Debug.Log("!!#");
     }
 
-    public void SaveGold(int gold)
-    {
-        _gold = gold;
-    }
-
-    public void SaveLevel(int level)
-    {
-        _level = level;
-    }
-
-    public void SaveDataAndExitGame()
+    public void SaveDataAndExitGame(int gold = 0, int exp = 0)
     {
         Debug.Log("Save data and exit this game.");
 
-        PlayerData.SetPlayerData(new PlayerData() {_gold = _gold, _level = _level });
+        PlayerData.SetPlayerData(new PlayerData() {_gold = gold, _level = exp });
     }
 }
