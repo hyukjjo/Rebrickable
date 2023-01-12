d45afd74cba525961f0ce078a227411401d02a1d
hyukjjo <hyukjjo@gmail.com>
2023-01-12 17:06:59 +0900

    chore: PlayerSpawner.cs
    안쓰는 코드 주석처리

--- Assets/Scripts/Game/Player/PlayerSpawner.cs
+++ Assets/Scripts/Game/Player/PlayerSpawner.cs
@@ -10,10 +10,10 @@ public class PlayerSpawner : MonoBehaviour
     void Start()
     {
         SpawnPlayer();
-        playerData = new PlayerData();
-        playerData._gold = 0;
-        playerData._level = 0;
-        PlayerData.SetPlayerData(playerData);
+        //playerData = new PlayerData();
+        //playerData._gold = 0;
+        //playerData._level = 0;
+        //PlayerData.SetPlayerData(playerData);
     }
 
     // Update is called once per frame
