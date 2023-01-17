30727597f92d012e854eb624a9fafe029daa46ab
hyukjjo <hyukjjo@gmail.com>
2023-01-17 17:18:18 +0900

    Update PopUpWarning.cs

--- Assets/Scripts/UI/PopUpWarning.cs
+++ Assets/Scripts/UI/PopUpWarning.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PopUpWarning : MonoBehaviour
 {
+    [SerializeField]
+    private string _content;
+    private Text _text;
+
     private void OnEnable()
     {
-        
+        if (_text == null)
+        {
+            _text = GetComponentInChildren<Text>();
+        }
+
+        _text.text = _content;
     }
 
     private void OnDisable()
