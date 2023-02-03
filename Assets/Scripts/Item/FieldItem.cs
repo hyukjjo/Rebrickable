177a5903e93c7b3ed70f912ee82a4984494d77e8
hyukjjo <hyukjjo@gmail.com>
2023-02-03 17:43:13 +0900

    Update FieldItem.cs

--- Assets/Scripts/Item/FieldItem.cs
+++ Assets/Scripts/Item/FieldItem.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum EFieldItemType
+{
+    MonsterKill = 0,
+    SpeedUpgrade,
+    MagnetUpgrade,
+    None
+}
+
+[System.Serializable]
+public class FieldItemData
+{
+    public EFieldItemType FieldItemType = EFieldItemType.None;
+    public Sprite Sprite;
+}
 
 public class FieldItem : MonoBehaviour
 {
+    private Image _image;
+    public FieldItemData CurrentData;
+
+    public List<FieldItemData> FieldItemDataList = new List<FieldItemData>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        _image = GetComponentInChildren<Image>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(FieldItemDataList.Count > 0)
+        {
+            CurrentData = FieldItemDataList[Random.Range(0, FieldItemDataList.Count + 1)];
+        }
+
+        if(CurrentData != null)
+        {
+            switch (CurrentData.FieldItemType)
+            {
+                case EFieldItemType.MonsterKill:
+                    GameManager.Instance.KillAllMonstersInField();
+                    break;
+                case EFieldItemType.SpeedUpgrade:
+                    GameManager.Instance.PlayerSpeedUp();
+                    break;
+                case EFieldItemType.MagnetUpgrade:
+                    GameManager.Instance.PlayerMagnetUp();
+                    break;
+                case EFieldItemType.None:
+                    Destroy(gameObject);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
