fd43d5eec6bda808e53e9d4db02fc6529c8fbe61
hyukjjo <hyukjjo@gmail.com>
2023-04-07 16:56:42 +0900

    Update MonsterSpawner.cs
    몬스터 spawn 방식 변경

--- Assets/Scripts/Game/Monster/MonsterSpawner.cs
+++ Assets/Scripts/Game/Monster/MonsterSpawner.cs
@@ -4,29 +4,51 @@ using UnityEngine;
 
 public class MonsterSpawner : Singleton<MonsterSpawner>
 {
+    [SerializeField]
+    private int _startingMonsterCount = 0;
+    [SerializeField]
+    private float _spawnTime = 0f;
+    [SerializeField]
+    private string _currentSpawnTarget = "Monster_Lv1";
+    private Coroutine _coroutine;
+
     // Start is called before the first frame update
-    void Start()                                           
-    {                                                      
-        InitSpawnMonster();                                
-    }                                                      
-                                                           
+    void Start()
+    {
+        InitSpawnMonster();
+    }
+
     // Update is called once per frame                     
-    void Update()                                          
-    {                                                      
-                                                           
-    }                                                      
-                                                           
-    void InitSpawnMonster()        
-    {                              
-        for(int i = 0; i < 10; i++)
+    void Update()
+    {
+
+    }
+
+    void InitSpawnMonster()
+    {
+        for (int i = 0; i < 10; i++)
         {
             SpawnMonster();
         }
+
+        _coroutine = StartCoroutine(SpawnMonsterCoroutine());
     }
 
     public void SpawnMonster()
     {
-        var monster = ObjectPoolManager.Instance.Spawn("Monster_Lv1");
+        var monster = ObjectPoolManager.Instance.Spawn(_currentSpawnTarget);
         monster.transform.position = new Vector2(Random.Range(-20, 20), Random.Range(-20, 20));
+    }
+
+    public void StopSpawnMonster()
+    {
+
+    }
+
+    private IEnumerator SpawnMonsterCoroutine()
+    {
+        SpawnMonster();
+
+        yield return new WaitForSeconds(_spawnTime);
     }
 }
